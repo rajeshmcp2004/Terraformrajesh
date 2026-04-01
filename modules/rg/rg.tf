2d60c3674908571306936bf78556f3692a1c1b6d
rajeshmcp2004 <69578536+rajeshmcp2004@users.noreply.github.com>
2026-04-01 14:06:18 +0530

    Update rg.tf

--- modules/rg/rg.tf
+++ modules/rg/rg.tf
@@ -1,1 +1,11 @@
-
+	resource "azurerm_resource_group" "rg" {
+	  count    = length(var.rg_names)
+	 
+	  name     = var.rg_names[count.index]
+	  location = var.rg_location
+	 
+	  tags = {
+	    environment = "Dev"
+	  }
+	}
+EOF
